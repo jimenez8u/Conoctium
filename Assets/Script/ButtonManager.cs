1719250fe469370f74266b76c50db84caf658edd
Pierrick Humbert <pierrick.humbert.68@gmail.com>
2017-11-11 12:40:33 -0500

    Menu version 2

--- Assets/Script/ButtonManager.cs
+++ Assets/Script/ButtonManager.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
+	Animator animator;
 
+	void Start () {
+		animator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+	}
+
+	public void Transition1 () {
+		
+		animator.SetTrigger("MoveMenuRight");
+
+	}
+	public void Transition2 () {
+
+		animator.SetTrigger("MoveMenuLeft");
+
+	}
 	public void NewGameBtn(string newGameLevel)
     {
 		
