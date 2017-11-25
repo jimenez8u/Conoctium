9370b03e569dd43268ee8d1d01ed150b10e33e8f
Loïc JIMENEZ <nier57@hotmail.fr>
2017-11-25 17:44:15 -0500

    Adding rotating levels

---
+++ Assets/Script/RollingBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBox : MonoBehaviour {
+    public float rotationSpeed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Transform>().Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+	}
+}
